fc441d5ed8a8f6dd0f425a91c0519169e37ebb1e
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-07-06 11:06:43 +0100

    Logs

--- src/app/hub/dashboard/page.tsx
+++ src/app/hub/dashboard/page.tsx
@@ -1,13 +1,21 @@
 //path: src\app\hub\dashboard\page.tsx
 
+"use client"
+
 import LoggedInRedirect from "@/app/components/generic/react/loggedInRedirect";
 import UICanvas from "@/app/components/generic/react/uiCanvas";
+import { useUser } from '@clerk/nextjs';
 
 
 export default function Dashboard() {
+    const user = useUser();
+
     return <>
         <LoggedInRedirect>
             <UICanvas tailwind="w-1/2 flex-col">
+                {user ? "Logged In User: " + user.user?.fullName 
+                        : "No User Logged In"}
+                <br />
                 This is the Dashboard
             </UICanvas>
         </LoggedInRedirect>
--- src/app/hub/page.tsx
+++ src/app/hub/page.tsx
@@ -1,13 +1,21 @@
 //path: src\app\hub\page.tsx
 
+"use client"
+
 import LoggedInRedirect from "@/app/components/generic/react/loggedInRedirect";
 import UICanvas from "@/app/components/generic/react/uiCanvas";
+import { useUser } from '@clerk/nextjs';
 
 
 export default function Hub() {
+    const user = useUser();
+    
     return <>
         <LoggedInRedirect>
             <UICanvas tailwind="w-1/2 flex-col">
+                {user ? "Logged In User: " + user.user?.fullName 
+                        : "No User Logged In"}
+                <br />
                 This is the Hub
             </UICanvas>
         </LoggedInRedirect>
