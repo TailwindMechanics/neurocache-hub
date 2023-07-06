765804c8cf84e8e99c41c2610c095321cd6d0515
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-07-06 13:14:11 +0100

    Removed subdomain

--- next.config.js
+++ next.config.js
@@ -1,41 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*',
-                    },
-                ],
-            },
-        ];
-    },
-    async rewrites() {
-        return {
-            beforeFiles: [
-                {
-                    source: '/:path*',
-                    has: [
-                        {
-                            type: 'host',
-                            value: 'hub.neurocache.ai',
-                        },
-                    ],
-                    destination: '/hub/:path*',
-                },
-            ],
-        };
-    },
-    // Set assetPrefix dynamically based on the HOST header
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.output.publicPath = `https://${process.env.HOST}${config.output.publicPath}`;
-        }
-        return config;
-    },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
