f969936039f034298a9ba863a9ad75f7107dc8b3
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-07-11 19:10:03 +0100

    Chat ui functional

--- src/components/chat/autoResizingTextArea.tsx
+++ src/components/chat/autoResizingTextArea.tsx
@@ -1,15 +1,14 @@
-//path: src\components\chat\autoResizingTextArea.tsx
-
-import { useRef, useEffect, ChangeEvent } from 'react';
+import { useRef, useEffect, ChangeEvent, KeyboardEvent } from 'react';
 
 
 interface AutoResizingTextareaProps {
     value: string;
+    onKeyDown: (event: KeyboardEvent<HTMLTextAreaElement>) => void;
     onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
     placeholder?: string;
 }
 
-const AutoResizingTextarea = ({ value, onChange, placeholder }: AutoResizingTextareaProps) => {
+const AutoResizingTextarea = ({ value, onChange, onKeyDown, placeholder }: AutoResizingTextareaProps) => {
     const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
     const resizeTextarea = () => {
@@ -32,17 +31,18 @@ const AutoResizingTextarea = ({ value, onChange, placeholder }: AutoResizingText
         resizeTextarea();
     }, [value]);
 
-    return <>
+    return (
         <textarea
             ref={textAreaRef}
             value={value}
             onChange={onChange}
+            onKeyDown={onKeyDown}
             onInput={resizeTextarea}
             className="overflow-hidden flex-grow w-full text-text-dark bg-bg rounded-lg border border-main-light resize-none text-2xl focus:outline-none focus:ring-0"
             placeholder={placeholder}
             maxLength={250}
         />
-    </>
+    );
 };
 
 export default AutoResizingTextarea;
--- src/components/chat/chatInput.tsx
+++ src/components/chat/chatInput.tsx
@@ -1,24 +1,62 @@
+import { useState, FormEvent, KeyboardEvent, useRef, useEffect } from 'react';
 import AutoResizingTextarea from './autoResizingTextArea';
-import { FC, useState, ChangeEvent } from 'react'
 
-
-const ChatInput: FC = () => {
+const ChatInput = () => {
     const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState<string[]>([]);
+    const [userHasScrolled, setUserHasScrolled] = useState(false);
+    const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
-    const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+        const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+        const scrollFromBottom = scrollHeight - (scrollTop + clientHeight);
+        const scrollThreshold = 40;  // Set lineHeight according to your needs
+    
+        if (scrollFromBottom > scrollThreshold) {
+            setUserHasScrolled(true);
+        } else {
+            setUserHasScrolled(false);
+        }
+    }
+
+    useEffect(() => {
+        if (!userHasScrolled && messagesEndRef.current !== null) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [messages]);
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setMessage(event.target.value);
     };
 
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        setMessages([...messages, message]);
+        setMessage('');
+    };
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            handleSubmit(event);
+        }
+    }
+
     return (
-        <div className="flex-grow mt-auto">
-            <form className='mt-auto flex-grow m-0 -mb-2 h-full'>
+        <div className="p-3 flex flex-col w-full h-full">
+            <div className="scrollbar-none px-3 pt-3 pb-2 overflow-y-auto flex-grow flex flex-col" onScroll={handleScroll}>
+                {/* Display the messages */}
+                {messages.map((msg, index) => 
+                    <p key={index} className={`p-1 ${index % 2 === 0 ? 'bg-main-light rounded-lg' : 'bg-main-dark rounded-lg'}`}>{msg}</p>)}
+                <div ref={messagesEndRef} />
+            </div>
+            <form onSubmit={handleSubmit} className='-mb-2'>
                 <label htmlFor="chat" className="hidden sr-only">Your message</label>
-                    <AutoResizingTextarea
-                        value={message}
-                        onChange={handleChange}
-                        placeholder="Unleash the fury, Mitch." />
-                <div className=''>
-                </div>
+                <AutoResizingTextarea
+                    value={message}
+                    onChange={handleChange}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Unleash the fury, Mitch." />
             </form>
         </div>
     )
