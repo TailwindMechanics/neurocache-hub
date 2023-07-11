43bffbd1c7640badfb733021f859c77a0944f395
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-07-11 23:38:28 +0100

    .

--- src/app/api/book-library/add/route.ts
+++ src/app/api/book-library/add/route.ts
@@ -1,7 +1,5 @@
 //path: src\app\api\book-library\add\route.ts
 
-import { db } from '@vercel/postgres';
-import { kv } from '@vercel/kv';
 import { z } from 'zod';
 
 
@@ -14,33 +12,33 @@ const addBookValidator = z.object({
 export async function POST(req: Request) {
     const body = await req.json();
     const { title, author, genre } = addBookValidator.parse(body);
-    const client = await db.connect();
+    // const client = await db.connect();
 
     // Check if book with same title and author already exists
-    const existingBook = await client.sql`SELECT * FROM books WHERE title=${title} AND author=${author};`;
+    // const existingBook = await client.sql`SELECT * FROM books WHERE title=${title} AND author=${author};`;
     
-    if (existingBook.rows.length > 0) {
-        // Book already exists, return OK without inserting
-        return new Response(`${title} already exists`, { status: 200 })
-    }
+    // if (existingBook.rows.length > 0) {
+    //     // Book already exists, return OK without inserting
+    //     return new Response(`${title} already exists`, { status: 200 })
+    // }
     
-    try {
-        // Insert the new book into the database and get the returned ID
-        const { rows: newBookRows } = await client.sql`
-            INSERT INTO books (title, author, genre) 
-            VALUES (${title}, ${author}, ${genre}) 
-            RETURNING *;`;
+    // try {
+    //     // Insert the new book into the database and get the returned ID
+    //     const { rows: newBookRows } = await client.sql`
+    //         INSERT INTO books (title, author, genre) 
+    //         VALUES (${title}, ${author}, ${genre}) 
+    //         RETURNING *;`;
         
-        if (newBookRows.length > 0) {
-            const newBook = newBookRows[0];
+    //     if (newBookRows.length > 0) {
+    //         const newBook = newBookRows[0];
 
-            // Add the new book to the KV store
-            await kv.set(newBook.id, newBook);
-        }
+    //         // Add the new book to the KV store
+    //         await kv.set(newBook.id, newBook);
+    //     }
         
-    } catch (error) {
-        return new Response(`${error}`, { status: 500 })
-    }
+    // } catch (error) {
+    //     return new Response(`${error}`, { status: 500 })
+    // }
 
     return new Response(`Successfully added ${title}`, { status: 200 });
 }
--- src/app/api/book-library/get-by-id/route.ts
+++ src/app/api/book-library/get-by-id/route.ts
@@ -1,8 +1,6 @@
 //path: src\app\api\book-library\get-by-id\route.ts
 
-import { db } from '@vercel/postgres';
 import type { Book } from "@/types"
-import { kv } from '@vercel/kv';
 import { z } from 'zod';
 
 
@@ -17,29 +15,29 @@ export async function GET(req: Request) {
     const { id } = getBookValidator.parse({ id: query.get('id') });
 
     // Try to get the book from KV store
-    const bookInCache = await kv.get<Book>(id);
+    // const bookInCache = await kv.get<Book>(id);
     
-    if (bookInCache !== null) {
-      return new Response(JSON.stringify({
-          message: `Retrieved ${bookInCache.title} from cache`,
-          book: bookInCache
-      }), { status: 200 });
-    }
+    // if (bookInCache !== null) {
+    //   return new Response(JSON.stringify({
+    //       message: `Retrieved ${bookInCache.title} from cache`,
+    //       book: bookInCache
+    //   }), { status: 200 });
+    // }
 
     // If book is not in KV store, get it from the database
-    const client = await db.connect();
-    const { rows } = await client.sql`SELECT * FROM books WHERE id=${id}`;
+    // const client = await db.connect();
+    // const { rows } = await client.sql`SELECT * FROM books WHERE id=${id}`;
 
-    if (rows.length === 0) {
-      return new Response(`No book found with this ID ${id}.`, { status: 404 });
-    }
+    // if (rows.length === 0) {
+    //   return new Response(`No book found with this ID ${id}.`, { status: 404 });
+    // }
 
     // Add the retrieved book to the cache for future requests
-    await kv.set(id, rows[0]);
+    // await kv.set(id, rows[0]);
     
     // Return the found book
-    const book = rows[0];
-    return new Response(JSON.stringify(book), { status: 200 });
+    // const book = rows[0];
+    // return new Response(JSON.stringify(book), { status: 200 });
 
   } catch (error) {
     if (error instanceof z.ZodError) {
