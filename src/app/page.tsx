cf76c9719b48a3381ad83f4731eb8fc2d3b22dfd
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-08-17 18:46:50 +0100

    Starting fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,63 +2,16 @@
 
 "use client";
 
-import colors from "@src/data/colors";
-import { FC, useCallback } from "react";
+import ReactFlowCanvas from "@src/components/react_flow/core/reactFlowCanvas";
+import { FC } from "react";
 import React from "react";
-import ReactFlow, {
-	BackgroundVariant,
-	useEdgesState,
-	useNodesState,
-	Background,
-	Connection,
-	addEdge,
-	Edge,
-	Controls,
-	MiniMap,
-} from "reactflow";
 
-const initialNodes = [
-	{
-		id: "1",
-		position: { x: 100, y: 0 },
-		data: { label: "God fucking dammit" },
-	},
-	{
-		id: "2",
-		position: { x: 100, y: 100 },
-		data: { label: "Fuck fucking this" },
-	},
-];
-
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
-export default function page() {
-	const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-	const onConnect = useCallback(
-		(params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
-		[setEdges],
-	);
-
+const page: FC = () => {
 	return (
-		<div style={{ width: "100vw", height: "100vh" }}>
-			<ReactFlow
-				nodes={nodes}
-				edges={edges}
-				onNodesChange={onNodesChange}
-				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-				fitView
-			>
-				<Controls />
-				<MiniMap />
-				<Background
-					variant={BackgroundVariant.Dots}
-					gap={12}
-					size={1}
-				/>
-			</ReactFlow>
-		</div>
+		<>
+			<ReactFlowCanvas />
+		</>
 	);
-}
+};
+
+export default page;
