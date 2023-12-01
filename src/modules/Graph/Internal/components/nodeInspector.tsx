c87dcef89d2e831ca2fe40b58319baeafbc26d96
Tailwind Mechanics <shea@tailwindmechanics.com>
2023-12-01 03:15:01 +0000

    Updated node inspector body font weight

--- src/modules/Graph/Internal/components/nodeInspector.tsx
+++ src/modules/Graph/Internal/components/nodeInspector.tsx
@@ -12,17 +12,17 @@ const NodeInspector: FC<NodeInspectorProps> = (props) => {
         <div className="rounded-lg bg-night-black p-2 font-mono font-extrabold leading-snug text-aqua-dark">
             <div className="mx-1 mb-2 mt-1 rounded-lg border-2 border-night-light pb-2 pt-1 text-center text-xl">
                 <p>Api Id</p>
-                <p className="font-medium leading-none text-night-body">
+                <p className="font-semi-bold leading-none text-night-body">
                     {props.nodeData.nodeId}
                 </p>
             </div>
             <div className="px-1.5">
                 Name:
-                <p className="font-medium text-night-body">
+                <p className="font-semi-bold text-night-body">
                     {props.nodeData.nodeName}
                 </p>
                 Description:
-                <p className="font-medium text-night-body">
+                <p className="font-semi-bold text-night-body">
                     {props.nodeData.body}
                 </p>
             </div>
